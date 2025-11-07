670e4d3e3405fee92d8bbcf22fa9598a7bc92853
AngeloGemelliGemelliAngelo <you@example.comgemeangelo27@gmail.com>
2025-11-07 11:28:29 +0100

    fix error with middlewares

--- controllers/emailsController.js
+++ controllers/emailsController.js
@@ -7,23 +7,17 @@ async function sendMail(req, res) {
     return res.status(400).json({ error: "Dati mancanti" });
   }
 
-  try {
-    const info = await transporter.sendMail({
-      from: '"Test App" <noreply@testapp.com>',
-      to,
-      subject,
-      text: message,
-      html: `<p>${message}</p>`,
-    });
+  const info = await transporter.sendMail({
+    from: '"Test App" <noreply@testapp.com>',
+    to,
+    subject,
+    text: message,
+    html: `<p>${message}</p>`,
+  });
 
-    console.log("Email inviata:", info.messageId);
-    res
-      .status(200)
-      .json({ message: "Email inviata con successo!", id: info.messageId });
-  } catch (error) {
-    console.error("Errore invio email:", error);
-    res.status(500).json({ error: "Errore durante l’invio dell’email" });
-  }
+  res
+    .status(200)
+    .json({ message: "Email inviata con successo!", id: info.messageId });
 }
 
 module.exports = { sendMail };
